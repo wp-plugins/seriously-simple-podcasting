5a94f7f717b338e373c0bc71403eeffd2f0fda3b
hlashbrooke <hlashbrooke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-11 14:06:24 +0000

    Seriously Simple Podcasting v1.3.4 - Fixed issue where site subtitle was being displayed in author field in feed
    git-svn-id: https://plugins.svn.wordpress.org/seriously-simple-podcasting/tags/1.3.4@679719 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ seriously-simple-podcasting.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Plugin Name: Seriously Simple Podcasting
+ * Version: 1.3.4
+ * Plugin URI: http://www.hughlashbrooke.com
+ * Description: An incredibly easy-to-use podcasting plugin.
+ * Author: Hugh Lashbrooke
+ * Author URI: http://www.hughlashbrooke.com
+ * Requires at least: 3.0
+ * Tested up to: 3.5.1
+ * 
+ * @package WordPress
+ * @subpackage SeriouslySimplePodcasting
+ * @author Hugh Lashbrooke
+ * @since 1.0.0
+ */
+
+/*
+ * Uses MediaElement.js for audio player
+ * http://mediaelementjs.com/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once( 'classes/class-seriously-simple-podcasting.php' );
+
+global $ss_podcasting;
+$ss_podcasting = new SeriouslySimplePodcasting( __FILE__ );
+
+if( is_admin() ) {
+	require_once( 'classes/class-seriously-simple-podcasting-admin.php' );
+	$ss_podcasting_admin = new SeriouslySimplePodcasting_Admin( __FILE__ );
+}
+
+require_once( 'seriously-simple-podcasting-functions.php' );
+require_once( 'classes/class-seriously-simple-podcasting-widget.php' );
+
+?>

c6f08d677fde50dad2b65f80afba93da9a182c28
hlashbrooke <hlashbrooke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-10 06:13:42 +0000

    Seriously Simple Podcasting v1.5.2 - Feed encoding update - Backwards compatibility fix - Episode download improvement
    git-svn-id: https://plugins.svn.wordpress.org/seriously-simple-podcasting/trunk@710747 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seriously-simple-podcasting-functions.php
+++ seriously-simple-podcasting-functions.php
@@ -8,7 +8,7 @@ if( ! function_exists( 'is_podcast_feed' ) ) {
 	 * @return boolean True if current page is podcast feed
 	 */
 	function is_podcast_feed() {
-		if( isset( $_GET['feed'] ) && $_GET['feed'] == 'podcast' ) {
+		if( isset( $_GET['feed'] ) && in_array( $_GET['feed'], array( 'podcast', 'itunes' ) ) ) {
 			return true;
 		}
 		return false;
@@ -17,12 +17,12 @@ if( ! function_exists( 'is_podcast_feed' ) ) {
 
 if( ! function_exists( 'is_file_download' ) ) {
 	/**
-	 * Check if file is being downloaded
+	 * Check if podcast file is being downloaded
 	 * @since  1.5
 	 * @return boolean True if file is being downloaded
 	 */
 	function is_file_download() {
-		if( ( isset( $_GET['download_file'] ) && $_GET['download_file'] == 'podcast' ) && isset( $_GET['episode'] ) ) {
+		if( isset( $_GET['podcast_episode'] ) ) {
 			return true;
 		}
 		return false;
@@ -60,18 +60,18 @@ if ( ! function_exists( 'ss_podcast' ) ) {
 
 		$defaults = array(
 			'echo' => true,
-			'link_title' => true, 
-			'title' => '', 
+			'link_title' => true,
+			'title' => '',
 			'content' => 'series',
 			'series' => '',
-			'before' => '<div class="widget widget_ss_podcast">', 
-			'after' => '</div><!--/.widget widget_ss_podcast-->', 
-			'before_title' => '<h3>', 
+			'before' => '<div class="widget widget_ss_podcast">',
+			'after' => '</div><!--/.widget widget_ss_podcast-->',
+			'before_title' => '<h3>',
 			'after_title' => '</h3>'
 		);
-		
+
 		$args = wp_parse_args( $args, $defaults );
-		
+
 		// Allow child themes/plugins to filter here.
 		$args = apply_filters( 'ss_podcast_args', $args );
 		$html = '';
@@ -84,7 +84,7 @@ if ( ! function_exists( 'ss_podcast' ) ) {
 		// The Display.
 		if ( ! is_wp_error( $query ) && is_array( $query ) && count( $query ) > 0 ) {
 			$html .= $args['before'] . "\n";
-			
+
 			if ( '' != $args['title'] ) {
 				$html .= $args['before_title'] . esc_html( $args['title'] ) . $args['after_title'] . "\n";
 			}
@@ -106,7 +106,7 @@ if ( ! function_exists( 'ss_podcast' ) ) {
 					$i++;
 
 					setup_postdata( $post );
-					
+
 					$class = 'podcast';
 
 					$title = get_the_title();
@@ -132,7 +132,7 @@ if ( ! function_exists( 'ss_podcast' ) ) {
 				}
 
 			} else {
-				
+
 				$i = 0;
 				foreach( $query as $series ) {
 
@@ -167,26 +167,26 @@ if ( ! function_exists( 'ss_podcast' ) ) {
 
 			wp_reset_postdata();
 		}
-		
+
 		// Allow themes/plugins to filter here.
 		$html = apply_filters( 'ss_podcast_html', $html, $query, $args );
-		
+
 		if ( $args['echo'] != true ) { return $html; }
-		
+
 		// Should only run if "echo" is set to true.
 		echo $html;
-		
+
 		do_action( 'ss_podcast_after', $args ); // Only if "echo" is set to true.
 	}
 }
 
 if ( ! function_exists( 'ss_podcast_shortcode' ) ) {
 	function ss_podcast_shortcode ( $atts, $content = null ) {
-		
+
 		$args = (array) $atts;
 
 		$defaults = array(
-			'title' => '', 
+			'title' => '',
 			'content' => 'series',
 			'series' => '',
 			'echo' => false,
@@ -205,4 +205,42 @@ if ( ! function_exists( 'ss_podcast_shortcode' ) ) {
 
 // Register shortcode
 add_shortcode( 'ss_podcast', 'ss_podcast_shortcode' );
+
+if ( ! function_exists('readfile_chunked')) {
+
+	/**
+	 * Reads file in chunks so big downloads are possible without changing PHP.INI - http://codeigniter.com/wiki/Download_helper_for_large_files/
+	 *
+	 * @param    string    file
+	 * @param    boolean    return bytes of file
+	 * @return   mixed
+	 */
+    function readfile_chunked( $file, $retbytes = true ) {
+
+		$chunksize = 1 * ( 1024 * 1024 );
+		$buffer = '';
+		$cnt = 0;
+
+		$handle = fopen( $file, 'r' );
+		if ( $handle === FALSE )
+			return FALSE;
+
+		while ( ! feof( $handle ) ) {
+			$buffer = fread( $handle, $chunksize );
+			echo $buffer;
+			ob_flush();
+			flush();
+
+			if ( $retbytes )
+				$cnt += strlen( $buffer );
+		}
+
+		$status = fclose( $handle );
+
+		if ( $retbytes && $status )
+			return $cnt;
+
+		return $status;
+    }
+}
 ?>
